451ff506d1b31dff06ffeeefb290bdf5b62518da
Shrikant Ramani <shrikantramani@Shrikants-MacBook-Air.local>
2024-07-20 22:45:15 +0530

    call accepted feature complete

--- client/src/pages/Room.jsx
+++ client/src/pages/Room.jsx
@@ -1,38 +1,64 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useSocket } from "../context/SocketProvider";
 import ReactPlayer from "react-player";
+import peer from "../service/peer";
 
 export default function RoomPage() {
   const socket = useSocket();
-  const [remoteSocketID, setRemoteSocketId] = useState(null);
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
   const [myStream, setMyStream] = useState(null);
 
   const handleUserJoined = useCallback(({ email, id }) => {
     console.log(`Email ${email} joined room`);
     setRemoteSocketId(id);
-  });
+  }, []);
 
   const handelCallUser = useCallback(async () => {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: true,
     });
-
+    const offer = await peer.getOffer();
+    socket.emit("user:call", { to: remoteSocketId, offer });
     setMyStream(stream);
+  }, [remoteSocketId, socket]);
+
+  const handleIncomingCall = useCallback(
+    async ({ from, offer }) => {
+      setRemoteSocketId(from);
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      setMyStream(stream);
+      console.log(`incoming call`, from, offer);
+      const ans = await peer.getAnswer(offer);
+      socket.emit("call:accepted", { to: from, ans });
+    },
+    [socket]
+  );
+  const handleCallAccepted = useCallback(({ from, ans }) => {
+    peer.setLocalDescription(ans);
+    console.log("Call accepted!");
   }, []);
+
   useEffect(() => {
     socket.on("user:joined", handleUserJoined);
+    socket.on("incoming:call", handleIncomingCall);
+    socket.on("call:accepted", handleCallAccepted);
 
     return () => {
-      socket.on("user:joined", handleUserJoined);
+      socket.off("user:joined", handleUserJoined);
+      socket.off("incoming:call", handleIncomingCall);
+      socket.off("call:accepted", handleCallAccepted);
     };
-  }, [socket, handleUserJoined]);
+  }, [socket, handleUserJoined, handleIncomingCall]);
 
   return (
     <div>
       <h1>Room Page</h1>
-      <h4>{remoteSocketID ? "Connected" : "No One in room"}</h4>
-      {remoteSocketID && <button onClick={handelCallUser}>Call</button>}
+      <h4>{remoteSocketId ? "Connected" : "No One in room"}</h4>
+      {remoteSocketId && <button onClick={handelCallUser}>Call</button>}
       {myStream && (
         <>
           <h1>My Stream</h1>
